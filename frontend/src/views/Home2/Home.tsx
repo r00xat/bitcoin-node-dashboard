8cd06c24a04b993ea759cffafdebc41fe39e394e
Jorge Montejo <jorgemon.lopez@gmail.com>
2024-06-12 15:47:34 +0000

    cards using tailwind css

--- frontend/src/views/Home2/Home.tsx
+++ frontend/src/views/Home2/Home.tsx
@@ -30,136 +30,258 @@ const Home = () => {
    return (
       <>
          <Header/>
-         <div className="container-fluid">
-            <div className="row">
-               <div className="col-12 col-md-3 p-0 content-wrapper">
-                  <div className="content custom-card">
-                     <div className="custom-card-title">
-                        Node
+         <br/>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 m-3">
+            <div className="bg-white rounded-md p-5">
+               <h1 className="text-2xl font-semibold text-center mb-2">
+                  Node
+               </h1>
+               <hr />
+               <ul className="mt-2">
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaIdCard className="me-1" />
+                           <span>
+                              Client
+                           </span>
+                        </div>
+                        <span>
+                           { nodeStore.client }
+                        </span>
                      </div>
-                     <hr className='m-2 mx-0'/>
-                     <div className="custom-card-body">
-                        <ul className="list-unstyled m-0">
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaIdCard className="me-1" />
-                                    <span className="fw-semibold">Client</span>
-                                 </div>
-                                 <span className="float-end">{nodeStore.client}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaArrowRightArrowLeft className="me-1" />
-                                    <span className="fw-semibold">Protocol</span>
-                                 </div>
-                                 <span className="float-end">{nodeStore.protocolVersion}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaNetworkWired className="me-1" />
-                                    <span className="fw-semibold">Port</span>
-                                 </div>
-                                 <span className="float-end">{nodeStore.port}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaClock className="me-1" />
-                                    <span className="fw-semibold">Uptime</span>
-                                 </div>
-                                 <span className="float-end">{formatSeconds(nodeStore.uptime)}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className="d-flex align-items-center">
-                                    <FaBullhorn className="me-1" />
-                                    <span className="fw-semibold">Services</span>
-                                 </div>
-                                 <span className="float-end d-flex flex-wrap justify-content-flex-end flex-row-reverse">
-                                    {
-                                       nodeStore.services.map((service, i) => {
-                                          return (
-                                             <span  key={i} className="badge bg-secondary ms-1 my-1">
-                                                {service}
-                                             </span>
-                                          )
-                                       })
-                                    }
-                                 </span>
-                              </div>
-                           </li>
-                        </ul>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaArrowRightArrowLeft className="me-1" />
+                           <span>
+                              Protocol
+                           </span>
+                        </div>
+                        <span>
+                           { nodeStore.protocolVersion }
+                        </span>
                      </div>
-                  </div>
-               </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaNetworkWired className="me-1" />
+                           <span>
+                              Port
+                           </span>
+                        </div>
+                        <span>
+                           { nodeStore.port }
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaClock className="me-1" />
+                           <span>
+                              Uptime
+                           </span>
+                        </div>
+                        <span>
+                           { formatSeconds(nodeStore.uptime) }
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaBullhorn className="me-1" />
+                           <span>
+                              Services
+                           </span>
+                        </div>
+                        <span className="max-w-80 flex flex-wrap justify-content-flex-end flex-row-reverse">
+                           {
+                              nodeStore.services.map((service, i) => {
+                                 return (
+                                    <span 
+                                       key={i} 
+                                       className="m-0.5 rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+                                    >
+                                       { service }
+                                    </span>
+                                 );
+                              })
+                           }
+                        </span>
+                     </div>
+                  </li>
+               </ul>
             </div>
-            <div className="row">
-              <div className="col-12 col-md-3 p-0 content-wrapper">
-                  <div className="content custom-card">
-                     <div className="custom-card-title">
-                        Blockchain
+            <div className=" bg-white rounded-md p-5">
+               <h1 className="text-2xl font-semibold text-center mb-2">
+                  Blockchain
+               </h1>
+               <hr/>
+               <ul className="mt-2">
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaLink className="me-1" />
+                           <span>
+                              Chain
+                           </span>
+                        </div>
+                        <span>
+                           { capitalizeFirst(blockchainStore.chain) }
+                        </span>
                      </div>
-                     <hr className='m-2 mx-0'/>
-                     <div className="custom-card-body">
-                        <ul className="list-unstyled m-0">
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaLink className="me-1" />
-                                    <span className="fw-semibold">Chain</span>
-                                 </div>
-                                 <span className="float-end">{capitalizeFirst(blockchainStore.chain)}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaHardDrive className="me-1" />
-                                    <span className="fw-semibold">Size</span>
-                                 </div>
-                                 <span className="float-end">{formatBytes(blockchainStore.size)}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaMicrochip className="me-1" />
-                                    <span className="fw-semibold">Difficulty</span>
-                                 </div>
-                                 <span className="float-end">{compactNumber(blockchainStore.difficulty)}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaGears className="me-1" />
-                                    <span className="fw-semibold">Hashrate</span>
-                                 </div>
-                                 <span className="float-end">{formatHashPerSecond(blockchainStore.hashRate)}</span>
-                              </div>
-                           </li>
-                           <li>
-                              <div className="d-flex align-items-center justify-content-between">
-                                 <div className='d-flex align-items-center'>
-                                    <FaCube className="me-1" />
-                                    <span className="fw-semibold">Last Block</span>
-                                 </div>
-                                 <span className="float-end">{formatLargeNumber(800000)}</span>
-                              </div>
-                           </li>
-                        </ul>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaHardDrive className="me-1" />
+                           <span>
+                              Size
+                           </span>
+                        </div>
+                        <span>
+                           { formatBytes(blockchainStore.size) }
+                        </span>
                      </div>
-                  </div>
-               </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaMicrochip className="me-1" />
+                           <span>
+                              Difficulty
+                           </span>
+                        </div>
+                        <span>
+                           { compactNumber(blockchainStore.difficulty) }
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaGears className="me-1" />
+                           <span>
+                              Hashrate
+                           </span>
+                        </div>
+                        <span>
+                           { formatHashPerSecond(blockchainStore.hashRate) }
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaCube className="me-1" />
+                           <span>
+                              Last Block
+                           </span>
+                        </div>
+                        <span>
+                           { formatLargeNumber(800000) }
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <FaClock className="me-1" />
+                           <span>
+                              Last Block Time
+                           </span>
+                        </div>
+                        <span>
+                           { formatSeconds(nodeStore.uptime) + ' ago'}
+                        </span>
+                     </div>
+                  </li>
+               </ul>              
+            </div>
+            <div className=" bg-white rounded-md p-5">
+               <h1 className="text-2xl font-semibold text-center mb-2">
+                  Network
+               </h1>
+               <hr/>
+               <ul className="mt-2">
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <span>
+                              IPv4
+                           </span>
+                        </div>
+                        <span 
+                           className="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                        >
+                           enabled
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <span>
+                              IPv6
+                           </span>
+                        </div>
+                        <span 
+                           className="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                        >
+                           enabled
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <span>
+                              Tor
+                           </span>
+                        </div>
+                        <span 
+                           className="rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+                        >
+                           disabled
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <span>
+                              Traffic Limit Set
+                           </span>
+                        </div>
+                        <span 
+                           className="rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+                        >
+                           disabled
+                        </span>
+                     </div>
+                  </li>
+                  <li>
+                     <div className="flex justify-between mb-1">
+                        <div className="flex items-center">
+                           <span>
+                              Traffic Limited
+                           </span>
+                        </div>
+                        <span 
+                           className="rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+                        >
+                           disabled
+                        </span>
+                     </div>
+                  </li>
+               </ul>
             </div>
          </div>
+         d
       </>
    );
 };
