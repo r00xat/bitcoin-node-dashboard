a43af6f2d71897c74ddaf9d8f75dff5ab686e83e
Jorge Montejo <jorgemon.lopez@gmail.com>
2024-07-23 09:31:34 +0000

    hide table fields on phone

--- frontend/src/views/Peers.tsx
+++ frontend/src/views/Peers.tsx
@@ -6,34 +6,50 @@ import { useEffect, useState } from "react";
 import clsx from "clsx";
 import React from "react";
 
+type TableField = {
+   name: string;
+   key: string;
+   show: 'always' | 'md' | 'lg' | 'xl' | '2xl';
+};
+
 function Peers() {
-   const [sortField, setSortField] = useState("Services");
-   const [order, setOrder] = useState("asc");
-   
+   const [sortField, setSortField] = useState("");
+   const [order, setOrder] = useState("");
+
    const peerStore = usePeerStore();
 
-   const tableFields = [{
+   const tableFields: TableField[] = [{
       name: 'Id',
-      key: 'id'
+      key: 'id',
+      show: 'always'
    }, {
       name: 'Address',
-      key: 'address'
+      key: 'address',
+      show: 'md'
    }, {
       name: 'Inbound',
-      key: 'inbound'
+      key: 'inbound',
+      show: 'always'
    }, {
       name: 'Services',
-      key: 'services'
+      key: 'services',
+      show: 'md'
    }, {
       name: 'Age',
-      key: 'conectionTime'
+      key: 'conectionTime',
+      show: 'always'
    }, {
       name: 'Client',
-      key: 'subversion'
+      key: 'subversion',
+      show: 'md'
    }, {
       name: 'Total Traffic',
-      key: 'totalbytes'
+      key: 'totalbytes',
+      show: 'always'
    }];
+
+   //To get the show property on showOrHideValue function
+   const tableFieldsHashMap = new Map(tableFields.map((field) => [field.key, field]));
 
    useEffect(() => {
       peerStore.fetch();
@@ -73,7 +89,7 @@ function Peers() {
          return (
             <React.Fragment key={index}>
                <span
-                  className="group relative m-0.5 rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 cursor-pointer"
+                  className="group relative m-0.5 rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 cursor-pointer whitespace-nowrap"
                >
                   {service}
                   {
@@ -89,73 +105,84 @@ function Peers() {
       });
    }
 
+   function showOrHideValue(fieldName: TableField['key']) {
+      const showProperty = tableFieldsHashMap.get(fieldName)?.show;
+      return clsx({
+        'hidden': showProperty === 'md',
+        'md:table-cell': showProperty === 'md',
+        'lg:table-cell': showProperty === 'lg',
+        'xl:table-cell': showProperty === 'xl',
+        '2xl:table-cell': showProperty === '2xl',
+      });
+    }
+
    return (
-      <div className="m-3 mt-7">
-         <Card>
-            <div className="flex flex-col">
-               <div className="-m-1.5 overflow-x-auto">
-                  <div className="p-1.5 min-w-full inline-block align-middle">
-                     <div className="border rounded-lg overflow-hidden">
-                        <table className="rounded-sm border-collapse table-auto w-full ">
-                           <thead className="bg-gray-200 rounded-2xl border-b">
-                              <tr>
-                                 {tableFields.map((field) => (
-                                    <th
-                                       key={field.key}
-                                       className="py-2.5 cursor-pointer"
-                                       onClick={() => {
-                                          if (sortField === field.key) {
-                                             setOrder(order === 'asc' ? 'desc' : 'asc');
-                                          } else {
-                                             setSortField(field.key);
-                                             setOrder('asc');
-                                          }
-                                       }}
-                                    >
-                                       <div className="flex justify-center items-center">
-                                          {field.name}
-                                          <span className="ml-1 flex flex-col text-gray-400">
-                                             <TiArrowSortedUp className={clsx({ "text-black": (sortField === field.key && order != 'asc') })}/>
-                                             <TiArrowSortedDown className={clsx({ "text-black": (sortField === field.key && order == 'asc') })}/>
-                                          </span>
-                                       </div>
-                                    </th>
-                                 ))}
-                              </tr>
-                           </thead>
-                           <tbody>
-                              {peerStore.peers.map((peer) => (
-                                 <tr key={peer.id} className="border-b last:border-0 hover:bg-gray-100 text-center odd:bg-gray-50">
-                                    <td className="py-2.5">
-                                       {peer.id}
-                                    </td>
-                                    <td>
-                                       {peer.address.split(':')[0]}
-                                    </td>
-                                    <td>
-                                       {peer.inbound ? "Yes" : "No"}
-                                    </td>
-                                    <td className="flex flex-wrap justify-center items-center py-2.5">
-                                       {formatServices(peer.services)}
-                                    </td>
-                                    <td>
-                                       {formatUnixTime(peer.conectionTime)}
-                                    </td>
-                                    <td>
-                                       {peer.subversion}</td>
-                                    <td>
-                                       {formatBytes(peer.bytessent + peer.bytesrecv)}
-                                    </td>
-                                 </tr>
-                              ))}
-                           </tbody>
-                        </table>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </Card>
-      </div>
+      <Card className="m-3 mt-7">
+         <div className="border rounded-lg overflow-hidden text-sm md:text-base">
+            <table className="table-auto w-full">
+               <thead className="bg-gray-200 rounded-2xl border-b">
+                  <tr>
+                     {tableFields.map((field) => (
+                        <th
+                           key={field.key}
+                           className={clsx('py-2.5 cursor-pointer', {
+                              'hidden': field.show === 'md',
+                              'md:table-cell': field.show === 'md',
+                              'lg:table-cell': field.show === 'lg',
+                              'xl:table-cell': field.show === 'xl',
+                              '2xl:table-cell': field.show === '2xl',
+                           })}
+                           onClick={() => {
+                              if (sortField === field.key) {
+                                 setOrder(order === 'asc' ? 'desc' : 'asc');
+                              } else {
+                                 setSortField(field.key);
+                                 setOrder('asc');
+                              }
+                           }}
+                        >
+                           <div className="flex justify-center items-center">
+                              {field.name}
+                              <span className="ml-1 flex flex-col text-gray-400">
+                                 <TiArrowSortedUp className={clsx({ "text-black": (sortField === field.key && order != 'asc') })} />
+                                 <TiArrowSortedDown className={clsx({ "text-black": (sortField === field.key && order == 'asc') })} />
+                              </span>
+                           </div>
+                        </th>
+                     ))}
+                  </tr>
+               </thead>
+               <tbody>
+                  {peerStore.peers.map((peer) => (
+                     <tr key={peer.id} className="border-b last:border-0 hover:bg-gray-100 text-center odd:bg-gray-50">
+                        <td className={clsx('py-2.5', showOrHideValue('id'))}>
+                           {peer.id}
+                        </td>
+                        <td className={showOrHideValue('address')}>
+                           {peer.address.split(':')[0]}
+                        </td>
+                        <td className={showOrHideValue('inbound')}>
+                           {peer.inbound ? "Yes" : "No"}
+                        </td>
+                        <td className={clsx(showOrHideValue('services'))}>
+                           <div className="flex flex-wrap justify-center items-center py-2.5">
+                              {formatServices(peer.services)}
+                           </div>
+                        </td>
+                        <td className={showOrHideValue('conectionTime')}>
+                           {formatUnixTime(peer.conectionTime)}
+                        </td>
+                        <td className={showOrHideValue('subversion')}>
+                           {peer.subversion}</td>
+                        <td className={showOrHideValue('totalbytes')}>
+                           {formatBytes(peer.bytessent + peer.bytesrecv)}
+                        </td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         </div>
+      </Card>
    );
 }
 
