764a587a2434838b8073f1ed54ab51c41ff00584
Jorge Montejo <jorgemon.lopez@gmail.com>
2024-08-20 10:31:28 +0000

    change login page style

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Logo from '@/icons/btc.svg';
 
 export default function Login() {
    const [rememberMe, setRememberMe] = useState(true);
@@ -8,31 +9,39 @@ export default function Login() {
    };
 
    return (
-      <div className="bg-gray-800 min-h-screen flex justify-center">
-         <form onSubmit={handleSubmit} className="flex flex-col justify-center">
-            <input
-               type="password"
-               placeholder="Password"
-               className="border border-gray-400 rounded p-2 ps-3"
-            />
-            <div className="mt-1">
-               <input
-                  type="checkbox"
-                  id="remembeMe"
-                  checked={rememberMe}
-                  onChange={() => setRememberMe(!rememberMe)}
-               />
-               <label htmlFor="rememberMe" className="text-white ps-1">
-                  Remember me
-               </label>
+      <div className="flex flex-row min-h-screen justify-center items-center">
+         <div className="flex justify-between flex-col bg-white h-60 p-6 rounded-lg">
+            <div className="flex flex-shrink-0 items-center">
+               <img className="h-10 w-auto me-1" src={Logo} style={{ transform: 'rotate(13.88deg)'}} />
+               <div className="text-2xl font-bold">
+                  Bitcoin Node Dasboard
+               </div>
             </div>
-            <button
-               type="submit"
-               className="bg-blue-500 text-white p-2 rounded mt-2"
-            >
-               Login
-            </button>
-         </form>
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center">
+               <input
+                  type="password"
+                  placeholder="Password"
+                  className="border border-gray-400 rounded p-2 ps-3"
+               />
+               <div className="mt-2">
+                  <input
+                     type="checkbox"
+                     id="rememberMe"
+                     checked={rememberMe}
+                     onChange={() => setRememberMe(!rememberMe)}
+                  />
+                  <label htmlFor="rememberMe" className="ps-1">
+                     Remember me
+                  </label>
+               </div>
+               <button
+                  type="submit"
+                  className="bg-blue-500 text-white p-2 rounded mt-2"
+               >
+                  Login
+               </button>
+            </form>
+         </div>
       </div>
    );
 }
