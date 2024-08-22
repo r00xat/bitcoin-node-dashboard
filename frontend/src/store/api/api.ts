f95140602c1f0f6a6e31a7da4c5014f41b9394e6
Jorge Montejo <jorgemon.lopez@gmail.com>
2024-08-22 15:27:55 +0000

    use axios interceptor to set the header

--- frontend/src/store/api/api.ts
+++ frontend/src/store/api/api.ts
@@ -4,9 +4,17 @@ import Cookies from 'js-cookie';
 const api = axios.create({
    baseURL: import.meta.env.VITE_BACKEND_URL,
    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + Cookies.get('token'),
+      'Content-Type': 'application/json'
    },
 });
+
+api.interceptors.request.use(function (config) {
+   const token = Cookies.get('token');
+   if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
+});
+
 
 export default api;
